289bae1ee4cb0a17858724056316c86f0589ea4c
Julio <julio@prestobr.com>
2026-03-03 09:32:45 -0300

    adicionando OpenApiConfig e removendo application.properties do git

---
+++ src/main/java/com/prestobr/financeiro/config/OpenApiConfig.java
@@ -0,0 +1,42 @@
+package com.prestobr.financeiro.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Value("${api.gateway.url}")
+    private String gatewayUrl;
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+
+        Server server = new Server();
+        server.setUrl(gatewayUrl + "/api/financeiro");
+        server.setDescription("API Financeiro");
+
+        return new OpenAPI()
+                .servers(List.of(server))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("Insira o token JWT obtido no login")
+                        )
+                );
+    }
+}
